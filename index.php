583f97da1cc8828ab542f3ffee0a46cfcc41006c
DUVAN\duvan <duvangamboa8@gmail.com>
2021-11-07 19:42:01 -0500

    no message

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php require_once("public/index.php") ?>
+<?php 
+/**require_once("public/index.php") */
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+<h1>DUVAN GAMBOA</h1>
+
+</body>
+</html>
